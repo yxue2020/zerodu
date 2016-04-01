3bbec7e310f072c9412862fc18ce973135de763a
huangchao <ftchao@local.com.cn>
2016-04-01 14:59:40 +0800

    add error_reporting

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
+
 defined('APP_DEBUG') or define('APP_DEBUG', true);
 defined('APP_ENV') or define('APP_ENV', 'devel');
+
+if (defined('APP_DEBUG')) {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+}
 
 require(__DIR__ . '/../vendor/autoload.php');
 
